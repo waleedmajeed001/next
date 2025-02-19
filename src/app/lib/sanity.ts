35ca83e5ad8ea42656f07d308431131311f41f0e
waleedmajeed001 <waleedmajeed630@gmail.com>
2025-02-19 15:39:53 +0500

    ful

--- src/app/lib/sanity.ts
+++ src/app/lib/sanity.ts
@@ -1,15 +1,16 @@
-import {createClient} from "next-sanity";
-import imageUrlBuilder from '@sanity/image-url';
+import { createClient } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
-    projectId: "5z3w1kk9",
-    dataset: "production",
-    apiVersion: "2022-03-25",
-    useCdn: true,
+  projectId: "5z3w1kk9",
+  dataset: "production",
+  apiVersion: "2022-03-25",
+  useCdn: true,
 });
 
-const builder = imageUrlBuilder(client)
+const builder = imageUrlBuilder(client);
 
-export function urlfor(source: any) {
-    return builder.image(source);
+export function urlfor(source: SanityImageSource) {
+  return builder.image(source);
 }
